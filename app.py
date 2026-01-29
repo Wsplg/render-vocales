c9296e556f51f3c8178296d53c7410c851bed940
Christian Jesús Travieso Alonso <christianjesustraviesoalonso@alumno.ieselrincon.es>
2026-01-29 19:27:50 +0000

    Simplify comprobar function and update message
    Removed emoji display and unnecessary code for vowel checking.

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def home():
         <input type="text" name="caracter" maxlength="1" required>
         <button type="submit">Comprobar</button>
     </form>
-    <p>Puedes probar letras y ver colores y emojis!</p>
+    <p>Puedes probar letras y ver colores!</p>
     """
 
 @app.route("/comprobar")
@@ -22,13 +22,3 @@ def comprobar():
 
     if caracter == "":
         return "<p style='color:red;'>No has escrito ningún carácter</p>"
-
-    if caracter in vocales:
-        return f"<p style='color:green; font-size:24px;'> {caracter.upper()} es una VOCAL </p>" \
-               "<a href='/'>Volver</a>"
-    else:
-        return f"<p style='color:blue; font-size:24px;'> {caracter.upper()} NO es una vocal</p>" \
-               "<a href='/'>Volver</a>"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
